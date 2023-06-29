980eec9be3eddcb18c57d213c98a45a91883949f
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-29 21:25:51 +0700

    memasukan Node Class

--- BinarySearchTree_023/BinarySearchTree_023.cpp
+++ BinarySearchTree_023/BinarySearchTree_023.cpp
@@ -8,4 +8,12 @@ public:
 	string info;
 	Node* leftchild;
 	Node* rightchild;
+
+	// Constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
 };
