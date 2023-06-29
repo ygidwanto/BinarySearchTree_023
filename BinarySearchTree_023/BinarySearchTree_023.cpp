40baee91dec4515cdbcb9fc84d2c94cc5c287418
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-06-29 21:55:22 +0700

    menambahkan node baru

--- BinarySearchTree_023/BinarySearchTree_023.cpp
+++ BinarySearchTree_023/BinarySearchTree_023.cpp
@@ -44,5 +44,10 @@ public:
 			ROOT = newNode; // Mark the new node as ROOT
 			return; // exit
 		}
+
+		if (element < parent->info)// If value in the data field of the new node is less than that of parent
+		{
+
+		}
 	}
 };
